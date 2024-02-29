a8920aaadb69d7baaba0e5f721a9b1f22d52d05f
Pansito <juanjescorciag@gmail.com>
2024-02-28 23:03:07 -0500

    Feature: navbar with flobite template and routes created

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     
     return render_template('index.html')
+
+@app.route('/navbar')
+def bar():
+    
+    return render_template('dias.html')
+@app.route('/ecco')
+def ecco():
+    
+    return render_template('ecco.html')
 if __name__ == "__main__":
     app.run(debug=True)

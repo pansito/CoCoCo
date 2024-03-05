db16f1dd4f92a00c03f0332d4244909ab837d811
Pansito <juanjescorciag@gmail.com>
2024-03-05 16:33:08 -0500

    Feature: footer inheritance and body padding removed

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -3,15 +3,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
-    return render_template('index.html')
+    return render_template('index.html',name='Congreso Colombiano de Combinatoria')
 
 @app.route('/dias')
 def bar():
     
-    return render_template('dias.html')
+    return render_template('dias.html',name='Dias Combinatoria')
 @app.route('/ecco')
 def ecco():
-    
-    return render_template('ecco.html')
+    return render_template('ecco.html', name='ECCO')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
